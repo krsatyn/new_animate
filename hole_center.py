d497e480b28f86a2a8e41fde678a12603bd65e64
Krsatyn <92187267+krsatyn@users.noreply.github.com>
2024-03-16 14:37:42 +0400

    update_hile_class

--- hole_center.py
+++ hole_center.py
@@ -48,31 +48,52 @@ class Hole_center():
             self.point_dict = self.input_coordinate_dict
     
     
-    # Генерация списка созданных обьектов
-    def generate_entity_point_group(self) -> dict:
+    # Конвертор координат под формат Vec3 (так же записывает количество отверстий в self.hole_counter:int и self.pints_counter:int количество точек)
+    def convertor_dict_coordinate_to_Vec3_coordinate(self,) -> dict:
+        '''Необходим для предворительного конвртировния координат'''
         
+        vec_3_point_dict = {'start':[], 'end':[]}
         point_dict = self.point_dict
+        for index_hole in range(len(point_dict['start'])):
+            
+            start_point_coordinate = ursina.Vec3((float(point_dict['start'][index_hole]['X']), # получение начальной координаты по оси X
+                                                  float(point_dict['start'][index_hole]['Y']), # получение начальной координаты по оси Y
+                                                  float(point_dict['start'][index_hole]['Z']))) # получение начальной координаты по оси Z)
+            
+            end_point_coordinate  =  ursina.Vec3((float(point_dict['end'][index_hole]['X']),   # получение конечной координаты по оси X
+                                                  float(point_dict['end'][index_hole]['Y']),   # получение конечной координаты по оси Y
+                                                  float(point_dict['end'][index_hole]['Z'])))   # получение конечной координаты по оси Z
+
+            vec_3_point_dict['start'].append(start_point_coordinate)
+            vec_3_point_dict['end'].append(end_point_coordinate)
+        
+        self.vec_3_point_dict = vec_3_point_dict
+        self.hole_counter = len(point_dict['start'])
+        self.pints_counter = len(point_dict['start']) * 2 # так как у отверстия есть вход и выход
+ 
+ 
+    # Генерация списка созданных обьектов
+    def generate_entity_point_group(self,) -> dict:
+        
+        # Получение конвертированных координат
+        point_dict = self.vec_3_point_dict
+        
+        # Получение количества отверситий
+        hole_counter = self.hole_counter
+        
+
+        # Словарь Entity sphere где ключ start входные отверстия а end выходные
         object_dict = {'start':[], 'end':[]}
         
-        for index in range(len(point_dict['start'])):
-            
-            coordinate_Vec3 = ( float(point_dict['start'][index]['X']),             # Конвертация X координатдля начальной позиции
-                                float(point_dict['start'][index]['Y']),             # Конвертация Y координаты для начальной позиции
-                                float(point_dict['start'][index]['Z']))             # Конвертация Z координаты для начальной позиции
+        for hole_index in range(hole_counter):
 
+            object_dict['start'].append(ursina.Entity(model='sphere',                             # Тип обьекта
+                                                      color=ursina.color.green,                   # Цвет обьекта
+                                                      position=point_dict['start'][hole_index]))  # Позиция    
             
-            object_dict['start'].append(ursina.Entity(model='sphere',             # Тип обьекта
-                                                      color=ursina.color.green,   # Цвет обьекта
-                                                      position=coordinate_Vec3))  # Позиция    
-            
-            coordinate_Vec3 = ( float(point_dict['end'][index]['X']),               # Конвертация X координаты для конечной позиции
-                                float(point_dict['end'][index]['Y']),               # Конвертация Y координаты для конечной позиции
-                                float(point_dict['end'][index]['Z']))               # Конвертация Z координаты для конечной позиции
-            
-            
-            object_dict['end'].append(ursina.Entity(model='sphere',               # Тип обьекта
-                                                    color=ursina.color.red,       # Цвет обьекта
-                                                    position=coordinate_Vec3))    # Позиция    
+            object_dict['end'].append(ursina.Entity(model='sphere',                               # Тип обьекта
+                                                    color=ursina.color.red,                       # Цвет обьекта
+                                                    position=point_dict['end'][hole_index]))      # Позиция    
             
             
         self.object_point_dict = object_dict
@@ -81,38 +102,41 @@ class Hole_center():
     
     
     # Создает связи между точками
-    def generate_connections_with_points(self, color_connection:ursina.color.Color=ursina.color.pink) -> list:
+    def generate_connections_with_points(self, color_connection:ursina.color.Color=ursina.color.pink,) -> list:
         
-        point_dict = self.point_dict
+        # Получение конвертированных координат
+        point_dict = self.vec_3_point_dict
+        
+        # Получение количества отверситий
+        hole_counter = self.hole_counter
+        
+        # Список Entity Cylinder выполняющие роль соединений
         connection_list = []
-        
-        print(type(point_dict['start']), "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
 
-        
-        # Создание точек и 
-        for index_hole in range(len(point_dict['start'])):
-            
-            start_point_coordinate = [float(point_dict['start'][index_hole]['X']), # получение начальной координаты по оси X
-                                      float(point_dict['start'][index_hole]['Y']), # получение начальной координаты по оси Y
-                                      float(point_dict['start'][index_hole]['Z'])] # получение начальной координаты по оси Z
-            
-            end_point_coordinate  =  [float(point_dict['end'][index_hole]['X']),   # получение конечной координаты по оси X
-                                      float(point_dict['end'][index_hole]['Y']),   # получение конечной координаты по оси Y
-                                      float(point_dict['end'][index_hole]['Z'])]   # получение конечной координаты по оси Z
+        # Создание связей 
+        for index_hole in range(hole_counter):
             
             # Получение координаты повнаправление цилиндра (координаты конца - координаты начала)
-            turn_coordinate =  np.subtract(np.array(end_point_coordinate), np.array(start_point_coordinate))
+            turn_coordinate =  np.subtract(np.array(point_dict['end'][index_hole]), np.array(point_dict['start'][index_hole]))
             
             # Создание обьекта Цилиндр по заданными параметрам
-            connection = ursina.Entity(model=ursina.Cylinder(6, direction=turn_coordinate), position=start_point_coordinate, color=color_connection)
+            connection = ursina.Entity(model=ursina.Cylinder(resolution=6,                              # Количество граней
+                                                             direction=(                                # Конечная координата:tuple(x,y,z)     
+                                                                    turn_coordinate[0],                 # Конечная координата X
+                                                                    turn_coordinate[1],                 # Конечная координата Y
+                                                                    turn_coordinate[2])),               # Конечная координата Z
+                                                            position=point_dict['start'][index_hole],   #Стартовая координата
+                                                            color=color_connection)                     # Цвет соединения 
             
-            connection_list = connection_list.append(connection)
+
+            connection_list.append(connection)
         
         self.connection_list = connection_list
         
         return connection_list
+       
         
-                    
+    # Основной метод запуска скрипта          
     def main(self,):
         
         #Список всех обьектов    
@@ -120,13 +144,19 @@ class Hole_center():
         
         # распаковываем json
         self.unpucking_json() 
-    
+
+        # Конвертация координат под формат ursina.Vec3
+        self.convertor_dict_coordinate_to_Vec3_coordinate()
+        
         #создаем список Entity точек центров отверстий 
-        entity_point_group = self.generate_entity_point_group() 
+        entity_point_group = self.generate_entity_point_group()
+        
+        #создаем список Entity цилиндров выполняюших роль соединения центров отверстий (соединяет start и end)  
         connections_points = self.generate_connections_with_points()
         
         #Заполнение списка обьектов
-        object_list.extend(entity_point_group,connections_points)
+        object_list.append(entity_point_group)
+        object_list.append(connections_points)
         
         return object_list
         
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-a = [1,2,3]
+import ursina
 
-a.extend([4,5])
-
+a = (3.13, 1.2, 3.5)
+a = ursina.Vec3(a)
 print(a)
